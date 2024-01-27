f2c48a12778b2a5a7b9e77ad6580b2bf346c852d
wensiet <wensietyt@gmail.com>
2024-01-27 14:01:05 +0300

    SentrySDK added

--- api/app.py
+++ api/app.py
@@ -3,6 +3,14 @@ from fastapi import FastAPI
 
 from api.products import router as products_router
 from api.authorization import router as authorization_router
+import sentry_sdk
+
+sentry_sdk.init(
+    dsn="https://glet_05f1d570dd5b0f3ed16431f86bac28ca@observe.gitlab.com:443/errortracking/api/v1/projects/54282896",
+
+    # Enable performance monitoring
+    enable_tracing=True,
+)
 
 app = FastAPI(title="API_NAME",
               description="API_DESC",
